7b006904f2cc88f72077c480cfbf68162ec470ef
scada-josh <scada.josh@gmail.com>
2024-01-15 16:55:30 +0700

    Create Script from MWA PC

---
+++ Assets/Script/TestOnPC.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestOnPC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // สร้างจาก เครื่อง PC กปน.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
