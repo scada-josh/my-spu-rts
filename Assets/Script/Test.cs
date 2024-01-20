92439c717a11c69b3256bfc7c3af234c0fa46851
scada-josh <scada.josh@gmail.com>
2024-01-20 09:57:30 +0700

    test commit

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    int a;
+    float b;
+    string c;
+    char d;
+    bool e;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 2;
+        int b = 3;
+
+        if(a>b){
+            Debug.Log("Hello");
+        } else if (b ==3 ) {
+        Debug.Log("Bye");
+        } else {
+            Debug.Log("xxx");
+        }
     }
 
     // Update is called once per frame
